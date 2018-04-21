c8614a742a16114350ac7712085e95ff7af00452
igority <igority@gmail.com>
2018-04-21 14:18:09 +0200

    v0.57

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
@@ -28,7 +27,9 @@
                     </button>
 
                     <!-- Branding Image -->
+
                     <a class="navbar-brand" href="{{ url('/') }}">
+                        <img class="logo-home-img" src="{{url('img/logo.png')}}" alt="">
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
