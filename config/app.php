8bcdda2f0b6f69a9e22fa69831b7a3e0c1e92ce2
igority <igority@gmail.com>
2018-04-28 19:49:11 +0200

    v0.58-1

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@ use FSR\Custom\CarbonFix;
 
 return [
 
-    'version' => '0.57',
+    'version' => '0.58',
     'upload_path' => env('UPLOAD_PATH', 'public/upload'),
     'master_admin' => env('MASTER_ADMIN', 'admin@admin.mk'),
     /*
