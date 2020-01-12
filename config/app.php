496f539ad3cf2740707ace27791b81240638a701
Igor <igority@gmail.com>
2020-01-12 19:06:51 +0100

    version 2.00

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@ use FSR\Custom\CarbonFix;
 
 return [
 
-    'version' => '1.00',
+    'version' => '2.00',
     'upload_path' => env('UPLOAD_PATH', 'public/upload'),
     'master_admin' => env('MASTER_ADMIN', 'sitesitimk@gmail.com'),
     /*
