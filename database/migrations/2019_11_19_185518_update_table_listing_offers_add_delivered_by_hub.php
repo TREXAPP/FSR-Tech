9337615f942bb8b5a3395d79224985f9a149c78c
Igor <igority@gmail.com>
2020-01-14 20:38:35 +0100

    update migration class name

--- database/migrations/2019_11_19_185518_update_table_listing_offers_add_delivered_by_hub.php
+++ database/migrations/2019_11_19_185518_update_table_listing_offers_add_delivered_by_hub.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTableListingOffersDeliveredByHub extends Migration
+class UpdateTableListingOffersAddDeliveredByHub extends Migration
 {
     /**
      * Run the migrations.
